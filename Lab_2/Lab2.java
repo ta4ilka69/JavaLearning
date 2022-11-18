a71fd8bf916a5e5b42cc126293922603bed8e98d
ta4ilka69 <artem_balin_2004@mail.ru>
2022-11-18 12:28:49 +0300

    final code

--- Lab_2/Lab2.java
+++ Lab_2/Lab2.java
@@ -4,12 +4,12 @@ import ru.ifmo.se.pokemon.*;
 public class Lab2 {
     public static void main(String[] args) {
         Battle b = new Battle();
-        Alomomola p1 = new Alomomola("Скрыга", 33);
-        Yanma p2 = new Yanma("Трупоед", 36);
-        Yanmega p3 = new Yanmega("Волколак", 43);
-        Oddish p4 = new Oddish("Волк",45);
-        Vileplume p5 = new Vileplume("Вожак",33);
-        Gloom p6 = new Gloom("Вампир",33);
+        Alomomola p1 = new Alomomola("Скрыга", 17);
+        Yanma p2 = new Yanma("Трупоед", 17);
+        Yanmega p3 = new Yanmega("Волколак", 17);
+        Oddish p4 = new Oddish("Волк",20);
+        Vileplume p5 = new Vileplume("Вожак",17);
+        Gloom p6 = new Gloom("Вампир",17);
         b.addAlly(p1);
         b.addAlly(p3);
         b.addAlly(p5);
