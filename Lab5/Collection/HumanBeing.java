0844c0d54a298d1f9771b3c45b8aef78c83d1f07
artem <artem_balin_2004@mail.ru>
2023-02-28 13:03:37 +0300

    lab5 with HB class started

---
+++ Lab5/Collection/HumanBeing.java
@@ -0,0 +1,70 @@
+package Collection;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HumanBeing {
+    private Integer id;
+    private String name;
+    private Coordinates coordinates;
+    private java.time.LocalDate creationDate;
+    private Boolean realHero;
+    private Boolean hasToothpick;
+    private Integer impactSpeed;
+    private String soundtrackName;
+    private float minutesOfWaiting;
+    private WeaponType weaponType;
+    private Car car;
+
+    public HumanBeing(
+            Integer id,
+            String name,
+            float x,
+            Double y,
+            Boolean realHero,
+            Boolean hasToothpick,
+            Integer impactSpeed,
+            String soundtrackName,
+            float minutesOfWaiting,
+            WeaponType weaponType,
+            Car car
+    ) {
+        this.id = id;
+        this.name = name;
+        this.coordinates = new Coordinates(x,y);
+        this.creationDate = LocalDate.now();
+        this.realHero = realHero;
+        this.hasToothpick = hasToothpick;
+        this.impactSpeed = impactSpeed;
+        this.soundtrackName = soundtrackName;
+        this.minutesOfWaiting = minutesOfWaiting;
+        this.weaponType = weaponType;
+        this.car = car;
+
+    }
+
+    public class Coordinates {
+        private float x;
+        private Double y;
+        public Coordinates(float x, Double y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public class Car {
+        private String name;
+        private boolean cool;
+        private static List<String> coolCars = new ArrayList<>();
+        public Car(String name){
+            this.name = name;
+        }
+    }
+
+    public enum WeaponType {
+        AXE,
+        RIFLE,
+        MACHINE_GUN;
+    }
+}
---
+++ Lab5/Collection/MyCollection.java
@@ -0,0 +1,39 @@
+package Collection;
+
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.PriorityQueue;
+
+public class MyCollection {
+    private PriorityQueue<HumanBeing> collection;
+
+    public MyCollection() {
+        this.collection = new PriorityQueue<>();
+    }
+
+    public void delete(int id) {
+        this.collection.remove(this.collection.toArray()[id]);
+    }
+
+    public void print(OutputStream out) {
+        Writer w = new PrintWriter(out);
+        var a = HumanBeing.class.getDeclaredFields();
+        for (HumanBeing c:collection
+             ) {
+            for (:
+                 ) {
+                
+            }
+        }
+    }
+
+
+    public int getSize() {
+        return this.collection.size();
+    }
+
+    public void clear() {
+        this.collection.clear();
+    }
+}
