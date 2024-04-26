2d3fe3234f257d82c4828bbdd31d84336690d0e4
ta4ilka69 <artem_balin_2004@mail.ru>
2024-04-26 14:21:11 +0300

    -

--- course1/src/Josephus.java
+++ course1/src/Josephus.java
@@ -2,19 +2,12 @@ import java.lang.Math;
 
 public class Josephus {
     public static int Josephus(int N, int k) {
-
-        // Initialize variables i and ans with 1 and 0 respectively.
         int i = 1, ans = 0;
-
-        // Run a while loop till i <= N
         while (i <= N) {
 
-            // Update the Value of ans and Increment i by 1
             ans = (ans + k) % i;
             i++;
         }
-
-        // Return required answer
         return ans + 1;
     }
 
@@ -26,8 +19,8 @@ public class Josephus {
             return 3*k;
         }
         if(n%3==1){
-            return 3*k+2;
+            return 3*k-2;
         }
-        return 3*k-2;
+        return 3*k-1;
     }
 }
--- course1/src/Main.java
+++ course1/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int N = 10, k = 2;
+        int N = 10, k = 3;
         int ans = Josephus.Josephus(N, k);
         System.out.println(ans);
         System.out.println(Josephus.Josephus3(N));
